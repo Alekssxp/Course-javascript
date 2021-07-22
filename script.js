ce2faca70c1511b1d03f966b764016935c16226e
Alekss_xp <alekss_xp@mail.ru>
2021-07-22 07:20:10 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let res = confirm('Тебе есть 18?');
-if (res == true) {
-    document.write('<h2 class = "dclass"> Привет Дяденька</h2>');
-} else {
-    document.write('<h2 class = "dclass"> Привет Малыш!!!</h2>');
-}
+console.log(2*4 === 8);
