bc555499b439f5f2209b599ea68e76214458c1bb
Alekss_xp <alekss_xp@mail.ru>
2021-07-22 06:42:32 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let res = confirm('Тебе есть 18?');
+if (res == true) {
+    document.write('<h2 class = "dclass"> Привет Дяденька</h2>');
+} else {
+    document.write('<h2 class = "dclass"> Привет Малыш!!!</h2>');
+}
