20192f5d3c643845fc8e7048978bee51e4edfc73
Alekss_xp <alekss_xp@mail.ru>
2021-07-22 17:11:23 +0300

    answer to practic_1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(2*4 === 8);
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    let lastNameFilms = prompt('Один из последних просмотренных фильмов?'),
+    gradeFilm = prompt('На сколько оцените его?');
+    let lastNameFilms1 = prompt('Один из последних просмотренных фильмов?'),
+    gradeFilm1 = prompt('На сколько оцените его?');
+
+
+    personalMovieDB.movies[lastNameFilms] = gradeFilm;
+    personalMovieDB.movies[lastNameFilms1] = gradeFilm1;
+
+console.log(personalMovieDB);
